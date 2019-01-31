4c81cdfc90d16a7c97577ce7c14bfd54a11d1306
Derek <nightwing917@hotmail.com>
2019-01-30 19:39:14 -0700

    setting up guess btn event listener

--- app.js
+++ app.js
@@ -7,4 +7,27 @@ let min = 1,
 
 
 // UI Elements    
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input')
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess btn
+// Note: black text means it is a string!!! so create a variable guess and use parseInt to change it to a number!
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    // console.log(guess) this would come back NaN or not a number!
+
+    // Validate
+    if (guess === NaN || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`);
+    }
+
+});
 
